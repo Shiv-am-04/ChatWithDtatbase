a6cab8a55fcf78cf71e4cd0ee146e53efec72ada
Shivam <shivamghuge01@gmail.com>
2024-11-25 23:17:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
-streamlit run app.py 
+mkdir -p ~/.streamlit/
+
+# Create credentials.toml
+cat <<EOL > ~/.streamlit/credentials.toml
+[general]
+email = "shivamghuge004@gmail.com"
+EOL
+
+# Create config.toml
+cat <<EOL > ~/.streamlit/config.toml
+[server]
+headless = true
+enableCORS = false
+port = $PORT
+EOL
